8596c635abff6a52069a3ed3eb7529eb914d5213
Daniel <daniel.zappold@gmx.de>
2021-12-01 20:12:58 +0100

    Solved day 1 😊

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map(String::toInt)
+            .zipWithNext { a: Int, b: Int -> a < b }
+            .count { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map(String::toInt)
+            .windowed(size = 3, step = 1, transform = List<Int>::sum)
+            .zipWithNext { a: Int, b: Int -> a < b }
+            .count { it }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
